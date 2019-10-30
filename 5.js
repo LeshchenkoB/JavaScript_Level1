d71ad67bae002018b5e81398d7a38810a923fd25
LeshchenkoB <lba84@mail.ru>
2019-10-31 00:24:30 +0700

    Выполнил задание №5

---
+++ 5.js
@@ -0,0 +1,14 @@
+//Задание №5 Нарисовать горку с помощью console.log (используя цикл for), как показано на рисунке,
+// только у вашей горки должно быть 20 рядов, а не 5:
+// x
+// xx
+// xxx
+// xxxx
+// xxxxx
+"use strict";
+
+let string = '';
+for (let i=0; i<20; i++){
+    string = string + 'x';
+    console.log(string);
+}
