108d2c6551276a97ded4c83a5d506632a7d32709
LeshchenkoB <lba84@mail.ru>
2019-12-14 19:03:13 +0700

    Написал функции hasWon() и isLineWon(a, b, c), которые проверяют выгрышную комбинацию значений в игровом поле

--- src/Game.js
+++ src/Game.js
@@ -83,5 +83,32 @@ class Game {
         this.settings.mapValues[row][col] = this.settings.phase;
         event.target.textContent = this.settings.phase;
     }
+    /**
+     * Проверка есть ли выигрышная ситуация на карте.
+     * @returns {boolean} Вернет true, если игра выиграна, иначе false.
+     */
+    hasWon() {
 
+        return this.isLineWon({ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }) ||
+            this.isLineWon({ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }) ||
+            this.isLineWon({ x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }) ||
+
+            this.isLineWon({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }) ||
+            this.isLineWon({ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }) ||
+            this.isLineWon({ x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }) ||
+
+            this.isLineWon({ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }) ||
+            this.isLineWon({ x: 0, y: 2 }, { x: 1, y: 1 }, { x: 2, y: 0 });
+    }
+    /**
+     * Проверка есть ли выигрышная ситуация на линии.
+     * @param {{x: int, y: int}} a 1-ая ячейка.
+     * @param {{x: int, y: int}} b 2-ая ячейка.
+     * @param {{x: int, y: int}} c 3-я ячейка.
+     * @returns {boolean} Вернет true, если линия выиграна, иначе false.
+     */
+    isLineWon(a, b, c) {
+        let value = this.settings.mapValues[a.y][a.x] + this.settings.mapValues[b.y][b.x] + this.settings.mapValues[c.y][c.x];
+        return value === 'XXX' || value === '000';
+    }
 }
